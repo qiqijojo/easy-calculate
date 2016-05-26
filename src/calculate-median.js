b07da2050c159a4d812707c93684da1e5bb55279
qq153377 <774948439@qq.com>
2016-05-26 11:59:50 +0800

    代码提交

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,7 +1,21 @@
 function calculate_median(arr) {
-  //请勿改动函数名
-  
-  jdhdjjdjdkcdmk;
+	arr.sort(sortNum);
+	var oArr = [];
+	for(var i=0;i<arr.length;i++){
+		if((i+1)%2==0){
+			oArr.push(arr[i]);
+		}	
+	}
+	var len = oArr.length;
+	if(len%2==0){
+		var mid = (oArr[len/2] + oArr[len/2-1])/2;	
+	}
+	else{
+		var mid = oArr[Math.floor(len/2)];	
+	}
+	return mid;	
 }
-
+function sortNum(a,b){
+	return a-b;
+	}
 module.exports = calculate_median;
